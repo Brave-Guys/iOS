bf5c90af3cab88a72187b152ccc30e7d9d2dc23f
combikms <combikms@naver.com>
2025-05-08 19:10:56 +0900

    Initial Commit

---
+++ iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by mac on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
